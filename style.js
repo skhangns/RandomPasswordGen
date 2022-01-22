383432fb97f69fb36ff1c980db6472d8f396e26e
Soheab Khan <skhangns@gmail.com>
2022-01-22 16:36:47 -0500

    added prompts in javascript

--- style.js
+++ style.js
@@ -1,15 +1,49 @@
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz"
+var upperCaseChar = "ABCDEFFGHIJKLMNOPQRSTUVWXYZ"
+var specialChar = "!@#$%^&*()_+?<>:{}[]"
+var numberChar = "0123456789"
+
 function getPassword(){
+    var passwordChars = "";
+    do{
+        var passwordLength = prompt("Choose the length of your password(btw 8-128 char",);
+       if(passwordLength<8 || passwordLength>128)
+        {alert("Password Length must between 8-128 characters")}
+    } while(passwordLength<8 || passwordLength>128)
+    if(passwordLength<8 || passwordLength>128){
+        alert("Password Length must between 8-128 characters")
+    }
+    var containsLowerCase = confirm("Would you like to include lowercase letters?");
+    if(containsLowerCase == true){
+        passwordChars += lowerCaseChar;
+       
+
+    }
+    var containsUpperCase = confirm("Would you like to include uppercase letters?");
+    if(containsUpperCase == true){
+       passwordChars += upperCaseChar;
+     
+    }
+    var containsSpecialChars = confirm("Would you like to include special letters?");
+    if(containsSpecialChars == true){
+       passwordChars += specialChar;
+       
+    }
+    var containsNumbers = confirm("Would you like to include numbers?");
+    if(containsNumbers == true || (containsLowerCase == false && containsSpecialChars == false && containsUpperCase == false)){
+       passwordChars += numberChar;
+       console.log(passwordChars);
+    }
+
+    console.log(passwordLength);
    
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?<>:{}[]";
-    var passwordLength = 20;
     var password = "";
 
     for (var i=0; i<passwordLength; i++){
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber,randomNumber+1);
+        var randomNumber = Math.floor(Math.random() * passwordChars.length);
+        password += passwordChars.substring(randomNumber,randomNumber+1);
         }
         document.getElementById("password").value = password
     }
 
-    
-
+   
